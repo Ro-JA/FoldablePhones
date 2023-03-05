d4fb67b73a04454ee85c6109523b806a8e2b6fbb
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-03-05 13:25:33 +0900

    not work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,32 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val foldablePhone = FoldablePhone(true)
+    foldablePhone.checkPhoneScreenLight()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+}
+
+open class Phone(var isScreenLightOn: Boolean = false) {
+    open fun switchOn() {
+        isScreenLightOn = true
+    }
+
+    open fun switchOff() {
+        isScreenLightOn = false
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(var isPhoneFoldable: Boolean = false) : Phone(false) {
+    override fun switchOn() {
+        if (isPhoneFoldable) isScreenLightOn = true
+    }
+
+    override fun switchOff() {
+        if (!isPhoneFoldable) isScreenLightOn = false
+    }
+
+
 }
